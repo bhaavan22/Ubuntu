98c4b82aafc470b50ff49a825bf219505cd92625
Lakshmi Bhaavan <bhaavan22@googlemail.com>
2016-04-28 18:55:15 +0530

    Update general.sh

--- general.sh
+++ general.sh
@@ -7,13 +7,3 @@ sudo rm -r /home/userName
 #Change hostname 
 sudo gedit /etc/hostname
 sudo gedit /etc/hosts
-
-#Touchpad not working for Fujitsu A514
-sudo nano /etc/default/grub
-#Replace 
-#GRUB_CMDLINE_LINUX_DEFAULT="quiet splash" 
-#with following
-#GRUB_CMDLINE_LINUX_DEFAULT="quiet splash i8042.notimeout i8042.nomux"
-#And then 
-sudo update-grub
-sudo reboot
