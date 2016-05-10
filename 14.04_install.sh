4c356dc14731f16c1b171042a142a0af8371aed1
Lakshmi Bhaavan <bhaavan22@googlemail.com>
2016-05-10 11:07:46 +0530

    Touch-pad indicator
    Touch-pad indicator Updated

--- 14.04_install.sh
+++ 14.04_install.sh
@@ -4,3 +4,8 @@ sudo modprobe -rfv iwlwifi
 sudo modprobe -v iwlwifi
 #Deactivate the power management of the card:
 sudo iwconfig wlan0 power off
+# Touch-pad indicator
+sudo add-apt-repository ppa:atareao/atareao
+sudo apt-get update
+sudo apt-get install touchpad-indicator
+# After that enable keyboard shortcut
